1b6098076d90f8ddfc1663881a9b0987a1f8dc53
mistuu <115686422+mistuu@users.noreply.github.com>
2023-07-24 17:31:08 +0530

    changes url

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use('/gets',getRoute)
 //     res.send('Hello World ')
 // })
 
-const mongoURI =  "mongodb+srv://rock:rock@cluster0.1uuakzo.mongodb.net/Demo?retryWrites=true&w=majority" 
+const mongoURI =  "mongodb+srv://rock:rock@cluster0.zskdioe.mongodb.net/Demo?retryWrites=true&w=majority" 
  mongoose.connect(mongoURI).then(() => console.log("connection sucessfull"));
 //listner port
 let port =process.env.PORT || 3000
